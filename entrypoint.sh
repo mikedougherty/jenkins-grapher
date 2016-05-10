d48746de20ef5ff81eacab9f9c9c8de9e8ec564e
Mike Dougherty <mike.dougherty@docker.com>
2016-05-10 15:11:49 -0700

    Cleanup entrypoint script a bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-DOT=dot
-dot -V >/dev/null 2>&1 || DOT=true
-
-filesize() {
-    if ! stat >/dev/null 2>&1; then
-        # Looks like linux
-        stat -c '%s' "$1"
-    else
-        # Probably a mac
-        stat -f '%z' "$1"
-    fi
-}
-
-/usr/bin/env python "$(dirname "${BASH_SOURCE}")/grapher.py" "$@" | tee "$OUTPUT_DIR/graph.dot" | "$DOT" -Tpng > "$OUTPUT_DIR/graph.png"
-[ "$( filesize "$OUTPUT_DIR/graph.png" )" -eq 0 ] && rm -f "$OUTPUT_DIR/graph.png"
-[ -n "$OUTPUT_FORMAT" ] && cat "$OUTPUT_DIR/graph.${OUTPUT_FORMAT}"
+/usr/bin/env python "$(dirname "${BASH_SOURCE}")/grapher.py" "$@" > "$OUTPUT_DIR/graph.dot"
+if which dot >/dev/null 2>&1 ; then
+    dot -Tpng "$OUTPUT_DIR/graph.dot" > "$OUTPUT_DIR/graph.png"
+fi
+[ -n "$OUTPUT_FORMAT" ] && [ -e "$OUTPUT_DIR/graph.${OUTPUT_FORMAT}" ] && cat "$OUTPUT_DIR/graph.${OUTPUT_FORMAT}"
